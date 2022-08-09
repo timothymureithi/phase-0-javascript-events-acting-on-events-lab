c0af5ff0c874e947e2c0788b24cc5a86f3b6ca5f
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-09 18:14:45 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerRight(){
+    var moveRight = dodger.style.left.replace('px',"");
+    let left = parseInt(moveRight,10);
+
+    if (left > 0 ){
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+function moveDodgerLeft(){
+    var moveLeft = dodger.style.left.replace('px',"");
+    let left = parseInt(moveLeft,10);
+
+    if (left > 0 ){
+        dodger.style.left = `${left - 1}px`
+    }
+}
